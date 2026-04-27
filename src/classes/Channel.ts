c26fb9de737166883405db5dfe97da7a7bf59874
ispik <ispik@ispik.dev>
2026-04-27 12:06:31 +0300

    refactor: remove unnecessary function
    Signed-off-by: ispik <ispik@ispik.dev>

--- src/classes/Channel.ts
+++ src/classes/Channel.ts
@@ -806,20 +806,6 @@ export class Channel {
   }
 
   /**
-   * Set slowmode value for the channel
-   * @param slowmode Slowmode value in seconds, between 0 and 21600
-   * @returns Updated channel data
-   */
-  async setSlowmode(slowmode: number): Promise<APIChannel> {
-    return await this.#collection.client.api.patch(
-      `/channels/${this.id as ""}`,
-      {
-        slowmode,
-      },
-    );
-  }
-
-  /**
    * Join a call
    * @param node Target node
    * @param forceDisconnect Whether to disconnect existing call
