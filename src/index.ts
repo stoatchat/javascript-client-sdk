0f2278a1d0fa81d2f02d3a2ee4d39c3031a11e02
Jacob Schlecht <dadadah@echoha.us>
2026-08-07 23:24:14 -0600

    fix: Export these types as they're used in for-web
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export type { ClientOptions, Session as PrivateSession } from "./Client.js";
 export * from "./classes/index.js";
 export * from "./collections/index.js";
 export { ConnectionState, EventClient } from "./events/EventClient.js";
+export { ProtocolV1 } from "./events/v1.js";
 export { BotFlags } from "./hydration/bot.js";
 export { ServerFlags } from "./hydration/server.js";
 export { UserBadges, UserFlags } from "./hydration/user.js";
