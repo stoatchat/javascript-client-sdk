444f36eed84b05911f25e634622d2d7af9b03874
Aki <aki@akiworks.xyz>
2026-09-24 17:24:55 +1200

    feat: add UseExternalEmojis permission (#194)
    Signed-off-by: Aki <aki@akiworks.xyz>

--- src/permissions/definitions.ts
+++ src/permissions/definitions.ts
@@ -95,8 +95,11 @@ export const Permission = {
   // Access server audit logs
   ViewAuditLogs: 2n ** 40n,
 
+  /// Use emoji from other servers
+  UseExternalEmojis: 2n ** 41n,
+
   // * Misc. permissions
-  // % Bits 41 to 52: free area
+  // % Bits 42 to 52: free area
   // % Bits 53 to 64: do not use
 
   // * Grant all permissions
@@ -153,4 +156,5 @@ export const DEFAULT_PERMISSION_SERVER =
   DEFAULT_PERMISSION +
   Permission.React +
   Permission.ChangeNickname +
-  Permission.ChangeAvatar;
+  Permission.ChangeAvatar +
+  Permission.UseExternalEmojis;
